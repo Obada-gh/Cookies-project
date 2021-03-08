ed9bfb3361533228c5993a9c12b6ccb28a32bd27
OBADA ALHAWJREH <obada7jaber7@gmail.com>
2021-03-08 07:51:11 +0200

    sorry i try my best

---
+++ js/app.js
@@ -0,0 +1,146 @@
+ 'use strict';
+
+
+
+let loc1 = {
+
+  locName: "seattle",
+  maxc : 65,
+  minc : 23,
+  avgco : 6.3,
+  cook : function(){
+
+  let randomg =  Math.floor(Math.random() * (this.maxc - this.minc + 1) + this.minc);
+  let numofcook = math.floor(this.avgco*randomg) ; 
+  }
+
+},
+storeAr: function() {
+
+  let ary = [];
+  for(let i = 0; i<14;i++){
+    ary[i] = this.cook();
+  }
+  ary[14] = 0 ;
+  for(let i = 0 ; i < 14; i++){
+
+    ary{14} += ary[i];
+  }
+  return ary;
+}
+};
+
+let loc2 = {
+
+  locName: "tokyo",
+  maxc : 24,
+  minc : 3,
+  avgco : 1.2,
+
+}
+cook : function(){
+
+  let randomg =  Math.floor(Math.random() * (this.maxc - this.minc + 1) + this.minc);
+  let numofcook = math.floor(this.avgco*randomg) ; 
+  }
+
+},
+storeAr: function() {
+
+  let ary = [];
+  for(let i = 0; i<14;i++){
+    ary[i] = this.cook();
+  }
+  ary[14] = 0 ;
+  for(let i = 0 ; i < 14; i++){
+
+    ary{14} += ary[i];
+  }
+  return ary;
+}
+};
+
+let loc3 = {
+
+  locName: "dubai",
+  maxc : 38,
+  minc : 20,
+  avgco : 3.7,
+  cook : function(){
+
+  let randomg =  Math.floor(Math.random() * (this.maxc - this.minc + 1) + this.minc);
+  let numofcook = math.floor(this.avgco*randomg) ; 
+  }
+
+},
+storeAr: function() {
+
+  let ary = [];
+  for(let i = 0; i<14;i++){
+    ary[i] = this.cook();
+  }
+  ary[14] = 0 ;
+  for(let i = 0 ; i < 14; i++){
+
+    ary{14} += ary[i];
+  }
+  return ary;
+}
+};
+
+let loc4 = {
+
+  locName: "paris",
+  maxc : 38,
+  minc : 20,
+  avgco : 2.3,
+  cook : function(){
+
+  let randomg =  Math.floor(Math.random() * (this.maxc - this.minc + 1) + this.minc);
+  let numofcook = math.floor(this.avgco*randomg) ; 
+  }
+
+},
+storeAr: function() {
+
+  let ary = [];
+  for(let i = 0; i<14;i++){
+    ary[i] = this.cook();
+  }
+  ary[14] = 0 ;
+  for(let i = 0 ; i < 14; i++){
+
+    ary{14} += ary[i];
+  }
+  return ary;
+}
+};
+
+let loc5 = {
+
+  locName: "lima",
+  maxc : 16,
+  minc : 2,
+  avgco : 4.6,
+  cook : function(){
+
+  let randomg =  Math.floor(Math.random() * (this.maxc - this.minc + 1) + this.minc);
+  let numofcook = math.floor(this.avgco*randomg) ; 
+  }
+
+},
+storeAr: function() {
+
+  let ary = [];
+  for(let i = 0; i<14;i++){
+    ary[i] = this.cook();
+  }
+  ary[14] = 0 ;
+  for(let i = 0 ; i < 14; i++){
+
+    ary{14} += ary[i];
+  }
+  return ary;
+}
+};
+
